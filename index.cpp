2fe633c14b9d24e34ad7d69ec6accd3e6d9419fb
fahad-faheem <fahadfaheem369@gmail.com>
2024-11-17 21:08:48 +0500

    This is another changes

--- index.cpp
+++ index.cpp
@@ -5,6 +5,9 @@
  int main() {
     cout << "Hello World" << endl;
     cout << "This is my another commit" << endl;
-
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
     return 0;
  }
