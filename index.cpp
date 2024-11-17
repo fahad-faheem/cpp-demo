9b4e3dc7b8b753ac275268f820bc60e8c6734da8
fahad-faheem <fahadfaheem369@gmail.com>
2024-11-17 19:09:12 +0500

    added for loop in this new branch

--- index.cpp
+++ index.cpp
@@ -5,6 +5,9 @@
  int main() {
     cout << "Hello World" << endl;
     cout << "This is my another commit" << endl;
-
+int a = 10;
+for (int i = 0; i < a; i++) {
+    cout << "Value of i is: " << i << endl;
+}
     return 0;
  }
