69ad200ab57b393f3d98e043651099e0b94baeb7
fahad-faheem <fahadfaheem369@gmail.com>
2024-11-17 21:27:01 +0500

    YEAH that's is!

--- index.cpp
+++ index.cpp
@@ -3,11 +3,9 @@
  using namespace std;
 
  int main() {
-    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;    
     cout << "This is my another commit" << endl;
-    string name;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Hello, " << name << "!" << endl;
     return 0;
+
+
  }
