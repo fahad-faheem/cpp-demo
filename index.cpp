38e837c422e2f1758aa04ececec8d23d22fc9ffa
fahad-faheem <fahadfaheem369@gmail.com>
2024-11-19 14:06:57 +0500

    just second time trying

--- index.cpp
+++ index.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+using namespace std;
 
+<<<<<<< Updated upstream
  #include <iostream>
  using namespace std;
 
@@ -6,7 +9,9 @@
     cout << "Hello World" << endl;    
     cout << "This is my another commit and another line yes it is" << endl;
     
+=======
+int main() {
+    cout << "No more hello world now lets go further and deeper" << endl;
+>>>>>>> Stashed changes
     return 0;
-
-
- }
+}
