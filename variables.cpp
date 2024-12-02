d1b9f2a99967b650a892313b0730ed567ae693f4
Fahad Faheem <fahadfaheem3344@gmail.com>
2024-12-02 14:24:44 +0500

    Pushing to github

--- variables.cpp
+++ variables.cpp
@@ -3,8 +3,10 @@
 
 int main (){
     std::string variable;
+    
     std::cout << "Give value to your own variable" << std::endl;
     std::cin >> variable;
+
     std::cout << "You've given " << variable << " to your variable " << std::endl;
     return 0;
 
