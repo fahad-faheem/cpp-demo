87fffa80b06f492a5a4f300720477b11f8a2be55
Fahad Faheem <fahadfaheem3344@gmail.com>
2024-12-26 09:28:50 +0500

    Very Basics lol

---
+++ VeryBasics/Basic.cpp
@@ -0,0 +1,24 @@
+// Declaration is just informing the conpiler that something exist 
+// Definitno is specifiying what it is ?
+// Initializing is assigning value to it 
+// 
+
+
+// Parmaters are variale that has been declared or defined inside function 
+// arguments are vaues assigned to parameters 
+
+// Actual parameters are variable very base variable that are sent to Function 
+// Formal paramters are variable that recieve the value from actual parametrs in 
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    cout << "Some very basics";
+
+
+
+
+    return 0;
+}
