7b8b677d05760cfae0483f5b2478b8dc0099ad37
Fahad Faheem <fahadfaheem3344@gmail.com>
2024-12-26 15:45:10 +0500

    Arrays basic definiton

---
+++ ArrayandPointers/Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int repeatednumer [] = {23, 45, 34, 45, 56, 76}; // arrays are used to  declare many variable in one line using same data types
+    //   indexs              0   1   2   3   4   5 
+    
+    cout << "Value att index 0 is " << repeatednumer[0] << endl;        // printing arrays
+    cout << "Value att index 1 is " << repeatednumer[1] << endl;
+    cout << "Value att index 2 is " << repeatednumer[2] << endl;
+    cout << "Value att index 3 is " << repeatednumer[3] << endl;
+    cout << "Value att index 4 is " << repeatednumer[4] << endl;
+    cout << "Value att index 5 is " << repeatednumer[5] << endl;
+
+
+
+
+    return 0;
+}
