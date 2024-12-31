750aa9536f0e5c70f9b5bf7d210eb69b753cfce7
Fahad Faheem <fahadfaheem3344@gmail.com>
2024-12-31 20:47:56 +0500

    Array

---
+++ ArrayandPointers/Array1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+    string arr [] = {"Black", "Day"};
+
+    cout << "This is " << arr[0] << endl;
+    cout << "This is " << arr[1] << endl;
+    cout << "This is " << arr[0] << arr [1] << endl;
+
+
+
+    return 0;
+}
