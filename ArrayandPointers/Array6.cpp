fba8636510cb52fe7d0a0a12493dd93f9ea382f9
Fahad Faheem <fahadfaheem3344@gmail.com>
2025-01-01 13:20:37 +0500

    Random for loop

---
+++ ArrayandPointers/Array6.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+
+
+    for (int i = 0; i < 5; i++){
+
+        cout << "Array element number " << i << " is " << i <<endl;
+    
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
