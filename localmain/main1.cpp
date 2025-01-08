982c38fc5a35efcae488bad334b556262900f589
Fahad Faheem <fahadfaheem3344@gmail.com>
2025-01-08 13:47:53 +0500

    Push to main branch from new fresh directory

--- localmain/main1.cpp
+++ localmain/main1.cpp
@@ -7,5 +7,6 @@ int main (){
 
 
     cout << "Main brach set up in new directory" << endl;
+    cout << "Main brach set up in new directory second time" << endl;
     cout << "Testing commit" << endl;
 }
